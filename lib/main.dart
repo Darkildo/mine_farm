3746e307a5c2a5342371b0d2c7e980669ed07fd1
D.ildo <34244107+Darkildo@users.noreply.github.com>
2022-10-01 09:12:50 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:purple_starter/runner_stub.dart'
+    if (dart.library.io) 'package:purple_starter/runner_io.dart'
+    if (dart.library.html) 'package:purple_starter/runner_web.dart' as runner;
+
+void main() => runner.run();
